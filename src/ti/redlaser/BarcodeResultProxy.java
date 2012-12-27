d3ed04600a0aa5149b88d33ce8d43faaaf82af27
John Pataki <john.pataki@logicallabs.com>
2012-12-27 11:33:18 -0800

    updating header comments and manifest info

--- src/ti/redlaser/BarcodeResultProxy.java
+++ src/ti/redlaser/BarcodeResultProxy.java
@@ -1,3 +1,16 @@
+//
+//  BarcodeResultProxy.java
+//  redlaser
+//
+//  Created by: 
+//  	Zsombor Papp	zsombor.papp@logicallabs.com
+//  	Logical Labs	titanium@logicallabs.com
+//
+//  Created on 11/1/12
+//
+//  Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
+//
+
 package ti.redlaser;
 
 import java.util.ArrayList;
